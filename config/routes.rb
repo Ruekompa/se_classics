fd06925c1d6560ef3a47283ab6f9aefd034dd560
Russell <rskompinski@gmail.com>
2019-02-26 23:27:33 -0500

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
  get 'vehicles/:id' => 'listings#vehicle_show', as: 'vehicle_listing'
  
  get 'admin' => 'vehicles#index'
+ devise_for :admins
  scope 'admin' do
   mount Ckeditor::Engine => '/ckeditor'
-  devise_for :admins
   resources :vehicles do
   	resources :vehicle_photos, :only => [:create, :destroy]
   end
