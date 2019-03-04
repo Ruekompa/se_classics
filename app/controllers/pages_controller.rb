230cd11c0d36ab73a73d80d47553e2711c4a066b
Russell <rskompinski@gmail.com>
2019-03-04 11:10:18 -0500

    fixed routes and some minor view changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
 	def home
-		
+		@vehicles = Vehicle.order("created_at DESC").limit(6)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
- get 'vehicles' => 'listings#vehicle_index'
- get 'vehicles/:id' => 'listings#vehicle_show', as: 'vehicle_listing'
+ get 'listings/vehicles' => 'listings#vehicle_index'
+ get 'listings/vehicles/:id' => 'listings#vehicle_show', as: 'vehicle_listing'
  
- get 'admin' => 'vehicles#index'
  devise_for :admins
- scope 'admin' do
-  mount Ckeditor::Engine => '/ckeditor'
-  resources :vehicles do
-  	resources :vehicle_photos, :only => [:create, :destroy]
+ get 'admin' => 'vehicles#index'
+ scope :admin do
+     resources :vehicles do
+  	     resources :vehicle_photos, :only => [:create, :destroy]
+  	     mount Ckeditor::Engine => '/ckeditor'
   end
 end
 
