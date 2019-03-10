ae6659a63cb3e0fe99d8aac0418ec2dff247ceec
Russell <rskompinski@gmail.com>
2019-03-10 14:29:44 -0400

    pages order of photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 	def home
 		@vehicles = Vehicle.order("created_at DESC").limit(6)
-		@vehicle_photos = VehiclePhoto.all
+		@vehicle_photos = VehiclePhoto.rank(:row_order).all
 	end
 
 end
