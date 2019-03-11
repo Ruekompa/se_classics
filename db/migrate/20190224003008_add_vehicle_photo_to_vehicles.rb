5cca8483b6a7f1c9717fd213512c6a204f64e668
Russell <rskompinski@gmail.com>
2019-03-11 00:34:33 -0400

    revert db

--- db/migrate/20190224003008_add_vehicle_photo_to_vehicles.rb
+++
@@ -1,5 +0,0 @@
-class AddVehiclePhotoToVehicles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :vehicles, :vehicle_photo, :string
-  end
-end
--- db/migrate/20190307173935_remove_vehicle_photos_from_vehicle.rb
+++
@@ -1,5 +0,0 @@
-class RemoveVehiclePhotosFromVehicle < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :vehicles, :vehicle_photos, :string
-  end
-end
