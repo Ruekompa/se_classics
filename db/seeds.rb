1209d3534188fef65563cd4d6931a74f6bfd8c88
Russell <rskompinski@gmail.com>
2019-03-11 00:50:34 -0400

    issue with seeds maybe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-admin = Admin.new([
-    {email: "drwoodgrain@gmail.com", 
+admin = Admin.new(
+    email: "drwoodgrain@gmail.com", 
     password: "tinman1963", 
-    password_confirmation: "tinman1963"},
-    {email: "rskompinski@gmail.com", 
-    password: "tinman1963", 
-    password_confirmation: "tinman1963"}
-])
+    password_confirmation: "tinman1963"
+)
 admin.save!
