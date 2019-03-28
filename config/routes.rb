c2d19406ffa381203978bb37802c80c63cf185e8
Russell <rskompinski@gmail.com>
2019-03-28 09:07:23 -0400

    added to registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   devise_for :admins, :skip => [:registrations]
 
   as :admin do
-    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
-    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+    get 'admins/edit' => 'admins/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'admins/registrations#update', :as => 'admin_registration'
   end
  
  get 'admin' => 'vehicles#index'
