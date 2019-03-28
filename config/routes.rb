59639c0cafb25b34d489b42bd2d06e33d3490ed8
Russell <rskompinski@gmail.com>
2019-03-27 20:28:47 -0400

    some ui updates to admin

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
 
   devise_for :admins, :skip => [:registrations]
+
   as :admin do
     get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
     put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
