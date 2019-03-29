6d8fde547b0e30e7be97d629fc61e6401899d025
Russell <rskompinski@gmail.com>
2019-03-29 16:27:10 -0400

    changed thumb sizes for uploader

--- app/uploaders/vehicle_photo_uploader.rb
+++ app/uploaders/vehicle_photo_uploader.rb
@@ -36,7 +36,7 @@ class VehiclePhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [200, 200]
+    process resize_to_fill: [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
