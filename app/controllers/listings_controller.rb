c3b87bee4c685b9aa5184675ee56e4d9b28f2c8a
Russell <rskompinski@gmail.com>
2019-03-30 12:14:53 -0400

    fixed dynamic title - a mystery of how it was deleted

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,36 +4,36 @@ class ListingsController < ApplicationController
  def vehicle_index
   	@pagy, @vehicles = pagy(Vehicle.order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
-  def vehicle_show
+ def vehicle_show
   	@vehicle = Vehicle.friendly.find params[:id]
   	@vehicle_photos = @vehicle.vehicle_photos.rank(:row_order).all
-  end
+ end
 
-  def vehicles_sold
+ def vehicles_sold
  	@pagy, @vehicles = pagy(Vehicle.where(sold: :true).order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
-  def vehicles_trucks
+ def vehicles_trucks
   	@pagy, @vehicles = pagy(Vehicle.where(type_of_vehicle: 'Truck').order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
-  def vehicles_cars
+ def vehicles_cars
   	@pagy, @vehicles = pagy(Vehicle.where(type_of_vehicle: 'Car').order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
-  def vehicles_motorcycles
+ def vehicles_motorcycles
   	@pagy, @vehicles = pagy(Vehicle.where(type_of_vehicle: 'Motorcycle').order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
-  def vehicles_classic
-  	@pagy, @vehicles = pagy(Vehicle.where("year < ?", Time.new.strftime("%Y").to_i - 25).order("created_at DESC"), items: 12)
+ def vehicles_classic
+  	@pagy, @vehicles = pagy(Vehicle.where("year <= ?", Time.new.strftime("%Y").to_i - 25).order("created_at DESC"), items: 12)
   	@vehicle_photos = VehiclePhoto.rank(:row_order).all
-  end
+ end
 
 end
